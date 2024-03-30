29b0fff4f1b1028ec9710ffa5d0ae395e7106e52
KizatovArman <kizatov.armashka.05@gmail.com>
2024-03-30 18:00:19 +0500

    [initial commit] new swiftui project

---
+++ Test Assignment/Test Assignment/Test_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_AssignmentApp.swift
+//  Test Assignment
+//
+//  Created by Arman on 30.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Test_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
